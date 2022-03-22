ef7fbde69b7e4c35b634107dfa44cec2adb20b45
LeventeWolf <wlevi2002@gmail.com>
2022-03-22 12:00:38 +0100

    Fix EventListener bug after reloading the component, close #10

--- react-client/src/components/shared/chat/ChatFooter.tsx
+++ react-client/src/components/shared/chat/ChatFooter.tsx
@@ -1,7 +1,10 @@
 import React, {useEffect, useRef, useState} from 'react';
 import SocketService from "../../../services/socketService";
 import SendIcon from '@mui/icons-material/Send';
+import MenuIcon from '@mui/icons-material/Menu';
+import ReturnIcon from '@mui/icons-material/KeyboardReturn';
 import './chatFooter.scss';
+import './../../../style/animations.scss';
 
 type PropTypes = {
     all_messages: any;
@@ -10,11 +13,8 @@ type PropTypes = {
 
 const ChatFooter: React.FC<PropTypes> = ( {all_messages, setMessages} ) => {
     const [partnerLeft, setPartnerLeft] = useState<boolean>(false);
-
+    const [isNavToggled, setNavToggled] = useState<boolean>(false);
     const inputChatRef = useRef<HTMLInputElement>(document.createElement("input"));
-    function handleLeave() {
-        window.location.reload(false);
-    }
 
     useEffect(() => {
         if (!SocketService.socket) return;
@@ -37,17 +37,6 @@ const ChatFooter: React.FC<PropTypes> = ( {all_messages, setMessages} ) => {
             SocketService.socket.off('partner_left');
             SocketService.socket.off('chat_message');
         });
-
-
-        if (inputChatRef.current) {
-            inputChatRef.current.addEventListener('keypress', sendMessageOnEnter);
-        }
-
-        async function sendMessageOnEnter(event: any) {
-            if (event.key === 'Enter') {
-                handleSendMessage();
-            }
-        }
 
         return () => {
             if (!SocketService.socket) return
@@ -76,6 +65,23 @@ const ChatFooter: React.FC<PropTypes> = ( {all_messages, setMessages} ) => {
         return;
     }
 
+    function sendMessageOnEnter(event: any) {
+        if (event.key === 'Enter') {
+            handleSendMessage();
+        }
+    }
+
+    function handleLeave() {
+        window.location.reload(false);
+    }
+
+    // Send messages by pressing key='Enter'
+    useEffect(() => {
+        if (inputChatRef.current) {
+            inputChatRef.current.addEventListener('keypress', sendMessageOnEnter);
+        }
+    }, [inputChatRef.current])
+
 
     if (partnerLeft) {
         return (
@@ -88,14 +94,33 @@ const ChatFooter: React.FC<PropTypes> = ( {all_messages, setMessages} ) => {
         )
     }
 
+    if (isNavToggled) {
+        return (
+            <div id="chat-footer">
+                <button type="button" className="btn btn-outline"
+                        onClick={() => setNavToggled(!isNavToggled)}>
+                    <ReturnIcon/>
+                </button>
+                <button onClick={handleLeave} className="btn btn-danger btn-leave-chat">
+                    Leave
+                </button>
+
+                <button disabled={true} type="button" className="btn btn-outline">
+
+                </button>
+            </div>
+        );
+    }
+
+
     return (
         <div id="chat-footer">
-            <button onClick={handleLeave} className="btn btn-danger btn-leave-chat">
-                Leave
+            <button type="button" className="btn btn-outline"
+                    onClick={() => setNavToggled(!isNavToggled)}>
+                <MenuIcon />
             </button>
 
-            <input type="text" className="form-control" placeholder="Say something nice!"
-                   ref={inputChatRef}/>
+            <input type="text" className="form-control" placeholder="Say something nice!" ref={inputChatRef}/>
 
             <button type="button" className="btn btn-outline"
                     onClick={handleSendMessage}>
