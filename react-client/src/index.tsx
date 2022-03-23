1ac098d1a22b0be964bbefac1a6428b4dec97a6e
LeventeWolf <wlevi2002@gmail.com>
2022-03-23 17:02:02 +0100

    Add alerts

--- react-client/src/index.tsx
+++ react-client/src/index.tsx
@@ -2,8 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.scss';
+import { transitions, positions, Provider as AlertProvider } from 'react-alert'
+// @ts-ignore
+import AlertTemplate from 'react-alert-template-basic'
 
-ReactDOM.render(
-        <App/>,
-  document.getElementById('root')
-);
+// optional configuration
+const options = {
+    position: positions.BOTTOM_CENTER,
+    timeout: 5000,
+    transition: transitions.FADE
+}
+
+const Root = () => (
+    <AlertProvider template={AlertTemplate} {...options}>
+        <App />
+    </AlertProvider>
+)
+
+ReactDOM.render(<Root />, document.getElementById('root'));
