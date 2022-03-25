5900d9e802c4438222bc237e4a8b0db414ad0984
LeventeWolf <wlevi2002@gmail.com>
2022-03-25 21:39:08 +0100

    Remove disconnected socket data

--- socketio-server/src/api/controllers/mainController.ts
+++ socketio-server/src/api/controllers/mainController.ts
@@ -1,20 +1,20 @@
 import {ConnectedSocket, OnConnect, OnDisconnect, SocketController, SocketIO,} from "socket-controllers";
 import {log} from "../../lib/logger";
 import {Socket, Server} from "socket.io";
-import socketData from "../services/socketLoggerService";
 import socketLogger from "../services/socketLoggerService";
 
 @SocketController()
 export class MainController {
     @OnConnect()
     public onConnection(@ConnectedSocket() socket: Socket, @SocketIO() io: Server) {
-        log(`New Socket connected: ${socket.id}`);
-        socketData.log_storeSocket(socket.id);
+        log(`[MainController] New Socket connected: ${socket.id}`);
+        socketLogger.log_storeSocket(socket.id);
         socketLogger.updateRooms(io.sockets.adapter.rooms);
     }
 
     @OnDisconnect()
     public async disconnect(@SocketIO() io: Server, @ConnectedSocket() socket: any) {
-        log('Disconnected socket ' + socket.id)
+        log(`[MainController] Disconnected socket ${socket.id}`)
+        socketLogger.removeSocket(socket.id);
     }
 }
